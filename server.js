0be60da0859efc0e973c5a7503bcd6d93d91945c
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-10-21 13:56:16 -1000

    moving on to sql commit, work to be done in notes file

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ var express = require('express');
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', './views');
-
-
-var tempData = [
-  { src : 'http://img01.deviantart.net/4c27/i/2013/143/e/1/dj_catbug_by_eilemont-d66c3gk.png', id : 1 },
-  { src : 'http://i.imgur.com/s85Xa.png', id : 2 },
-  { src : 'http://shirtoid.com/wp-content/uploads/2012/09/dead-link.jpg', id : 3 },
-  { src : 'http://shirtoid.com/wp-content/uploads/2015/06/i-know-html-how-to-meet-ladies.jpg', id : 4 }
-];
-
 app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {
+    tempData : [
+      { src : 'http://img01.deviantart.net/4c27/i/2013/143/e/1/dj_catbug_by_eilemont-d66c3gk.png', id : 1, descrip : 'img yo', author : 'someguy' },
+      { src : 'http://i.imgur.com/s85Xa.png', id : 2, descrip : 'img yo', author : 'someguy' },
+      { src : 'http://shirtoid.com/wp-content/uploads/2012/09/dead-link.jpg', id : 3, descrip : 'img yo', author : 'someguy' },
+      { src : 'http://shirtoid.com/wp-content/uploads/2015/06/i-know-html-how-to-meet-ladies.jpg', id : 4, descrip : 'img yo', author : 'someguy' }
+]
+  });
 });
 
 app.get('/gallery', function (req, res) {
