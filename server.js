6c6bc33bd9e77ed1614dd0795e2284467908eedc
SkinnyFatMan <SkinnyFatMan@gmail.com>
2015-10-28 19:46:54 -1000

    WebStorm commit - Authentication working. Still need a new post button, paused work on method-override, need login/logout button, working on put request

--- server.js
+++ server.js
@@ -1,15 +1,29 @@
 //REQUIRED STUFF
 var express = require('express');
 var app = express();
+var session = require('express-session');
 var bodyParser = require('body-parser');
+var flash = require('connect-flash');
+var passport = require('passport');
+var localStrategy = require('passport-local');
 var db = require('./models');
 var Post = db.post;
 var methodOverride = require('method-override');
 //var User = db.user;
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended : true }));
 app.set('view engine', 'jade');
 app.set('views', './views');
+app.use(session(
+    {
+      secret : 'poundbutt for the win',
+      resave : false,
+      saveUninitialized : true
+    }
+));
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static('public'));
 app.use(methodOverride(function(req, res) {
   if (req.body && typeof req.body === 'object' && '_method' in req.body) {
@@ -20,6 +34,44 @@ app.use(methodOverride(function(req, res) {
     return method;
   }
 }));
+passport.serializeUser(function(user, done) {
+  done(null, JSON.stringify(user));
+});
+passport.deserializeUser(function(obj, done) {
+  done(null, JSON.parse(obj));
+});
+passport.use(new localStrategy(
+    function(username, password, done) {
+      User.findOne({ username : username }, function (err, user) {
+        if (err) { return done(err); }
+        if (!user) {
+          return done(null, false, { message : 'Incorrect username.' });
+        }
+        if (!user.validPassword(password)) {
+          return done(null, false, { message : 'Incorrect password.' });
+        }
+        return done(null, user);
+      });
+    }
+));
+
+
+app.post('/login',
+    passport.authenticate('local', { successRedirect : '/gallery',
+      failureRedirect : '/login',
+      failureFlash : true })
+);
+
+app.get('/login', function(req,res) {
+  res.render('login', { user : req.user, messages : req.flash('error') });
+});
+
+app.get('/logout', function(req,res) {
+  req.logout();
+  res.redirect('/');
+});
+
+
 
 app.get('/', function (req, res) {
   Post.findAll().
@@ -39,7 +91,7 @@ app.get('/gallery', function (req, res) {
     });
 });
 
-app.get('/gallery/new', function(req, res) {
+app.get('/gallery/new', ensureAuthenticated, function(req, res) {
   res.render('gallery_new');
 });
 
@@ -58,7 +110,7 @@ app.post('/gallery/new', function (req, res) {
       });
 });
 
-app.get('/gallery/:id/edit', function(req, res) {
+app.get('/gallery/:id/edit', ensureAuthenticated, function(req, res) {
   Post.find({where : {'id': req.params.id}}).then(
     function(postById){
       res.render('gallery_edit', {author : postById.author, descrip : postById.descrip, src : postById.src});
@@ -77,9 +129,14 @@ app.get('/gallery/:id/edit', function(req, res) {
 //  }, {where : {'id': req.params.id}});
 //});
 
-app.delete('/gallery/:id', function (req, res) {
+app.delete('/gallery/:id', ensureAuthenticated, function (req, res) {
   res.send('DELETE GALLERY PAGE');
 });
+
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login');
+}
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
@@ -89,3 +146,16 @@ var server = app.listen(3000, function () {
   console.log('_Architekt listening at http://%s:%s', host, port);
 });
 
+var User = {
+  findOne : function (opts, cb) {
+    var user = {
+      id : 1,
+      username : opts.username,
+      password : 'poundbutt',
+      validPassword : function (password) {
+        return (password === 'poundbutt');
+      }
+    };
+    cb( null, user );
+  }
+};
